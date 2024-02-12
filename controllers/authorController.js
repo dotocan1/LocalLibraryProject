436c97ef6f7616846a392ce07c40a06a1b75813e
Dominik Otocan <dominik.otocan1@gmail.com>
2024-02-12 15:46:08 +0100

    updating author works now

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -148,12 +148,74 @@ exports.author_delete_post = asyncHandler(async (req, res, next) => {
 });
 
 
-// Display Author update form on GET.
+// Display author update form on GET.
 exports.author_update_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Author update GET");
+  const author = await Author.findById(req.params.id).exec()
+  // Get book, authors and genres for form.
+  // const [book, allAuthors, allGenres] = await Promise.all([
+  //   Book.findById(req.params.id).populate("author").exec(),
+  //   Author.find().sort({ family_name: 1 }).exec(),
+  //   Genre.find().sort({ name: 1 }).exec(),
+  // ]);
+
+  if (author === null) {
+    // No results.
+    const err = new Error("Author not found");
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render("author_form", {
+    title: "Update author",
+    author: author,
+  });
 });
 
-// Handle Author update on POST.
-exports.author_update_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Author update POST");
-});
+
+// Handle author update on POST.
+exports.author_update_post = [
+  // Validate and sanitize fields.
+  body("first_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("First name must be specified.")
+    .isAlphanumeric()
+    .withMessage("First name has non-alphanumeric characters."),
+  body("family_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Family name must be specified.")
+    .isAlphanumeric()
+    .withMessage("Family name has non-alphanumeric characters."),
+  body("date_of_birth", "Invalid date of birth")
+    .optional({ values: "falsy" })
+    .isISO8601()
+    .toDate(),
+  body("date_of_death", "Invalid date of death")
+    .optional({ values: "falsy" })
+    .isISO8601()
+    .toDate(),
+
+  // Process request after validation and sanitization.
+  asyncHandler(async (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a Author object with escaped/trimmed data and old id.
+    const author = new Author({
+      first_name: req.body.first_name,
+      family_name: req.body.family_name,
+      date_of_birth: req.body.date_of_birth,
+      date_of_death: req.body.date_of_death,
+      _id: req.params.id, // This is required, or a new ID will be assigned!
+    });
+
+    // Data from form is valid. Update the record.
+    const updatedAuthor = await Author.findByIdAndUpdate(req.params.id, author, {});
+    // Redirect to author detail page.
+    res.redirect(updatedAuthor.url);
+
+  }),
+];
