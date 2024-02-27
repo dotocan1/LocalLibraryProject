a9eb4523142b8d2151f0a1a92419ece6b14238ef
Dominik Otocan <dominik.otocan1@gmail.com>
2024-02-27 13:01:12 +0100

    posting complaints work

--- controllers/votingController.js
+++ controllers/votingController.js
@@ -1,13 +1,14 @@
 const BookInstance = require("../models/complaint.js");
 const asyncHandler = require("express-async-handler");
-
+const User = require("../models/user");
+const Complaint = require("../models/complaint");
 const { body, validationResult } = require("express-validator");
 
 exports.complaint_post = [
   // Validate and sanitize the name field.
-  body("name", "Complaint name must contain at least 3 characters")
+  body("description", "Complaint description must contain at least 30 characters")
     .trim()
-    .isLength({ min: 30 })
+    .isLength({ min: 2 })
     .escape(),
 
   // Process request after validation and sanitization.
@@ -18,18 +19,21 @@ exports.complaint_post = [
     // Create a complaint object with escaped and trimmed data.
     const complaint = new Complaint({
       description: req.body.description,
-      user: req.body.user,
+      user: req.body.selectedUser,
     });
 
     if (!errors.isEmpty()) {
       // There are errors. Render the form again with sanitized values/error messages.
+      const allUsers = await User.find().sort({ name: 1 }).exec();
       res.render("voting", {
         errors: errors.array(),
+        user_list: allUsers,
       });
       return;
     } else {
       // Data from form is valid.
       await complaint.save();
+      // console.log(`hey this is the complaint + ${complaint}`)
       // New complaint saved. Redirect to voting  page.
       res.redirect('/voting');
     }
--- routes/voting.js
+++ routes/voting.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var router = express.Router();
-//var votingController = require('../controllers/votingController')
+const User = require("../models/user");
+var votingController = require('../controllers/votingController')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('voting');
+router.get('/', async function (req, res, next) {
+  // Get all users
+  const allUsers = await User.find().sort({ name: 1 }).exec();
+  res.render('voting', { user_list: allUsers });
 });
 
 // POST a complaint
+router.post("/post/complaint", votingController.complaint_post);
 
 // POST yellow card
 
